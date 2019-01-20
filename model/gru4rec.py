ffd43dc16bf08c041dc8cd556457cc6dff8294bd
Patricio Cerda Mardini <pcerdam@uc.cl>
2019-01-20 19:24:21 -0300

    consolidated

--- model/gru4rec.py
+++ model/gru4rec.py
@@ -1,59 +1,38 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# #**Proyecto - Sistemas Recomendadores - IIC3633**
-# 
-# ## Implementación en Keras de Session-Based RNNs for Recommendation
-# 
-# ### Utilizacion de Dwell Time como feature implicita para mejor rendimiento
-
-# In[ ]:
-
-
 import os
 import sys
-import subprocess
 import math
-import pandas as pd
-import numpy as np
-import sklearn
 import psutil
+import sklearn
 import humanize
+import warnings
+import subprocess
+import numpy as np
+import pandas as pd
 import GPUtil as GPU
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+from sklearn.metrics import recall_score
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 from tensorflow.python.client import device_lib
-
 
 import tensorflow as tf
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 
-import warnings
-
 import keras
 import keras.backend as K
 from keras.utils import to_categorical
-from keras.losses import cosine_proximity, categorical_crossentropy
 from keras.models import Model, Sequential
+from keras.callbacks import ModelCheckpoint
 from keras.initializers import glorot_uniform
 from keras.layers.core import Permute, Reshape, RepeatVector
+from keras.losses import cosine_proximity, categorical_crossentropy
 from keras.layers import Input, Dense, Dropout, CuDNNGRU, Embedding, concatenate, Lambda, multiply, merge, Flatten
-from keras.callbacks import ModelCheckpoint
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 class SessionDataset:
+    """Credit to yhs-968/pyGRU4REC."""    
     def __init__(self, data, sep='\t', session_key='SessionId', item_key='ItemId', time_key='Time', n_samples=-1, itemmap=None, time_sort=False):
         """
         Args:
@@ -78,7 +57,6 @@ class SessionDataset:
         self.click_offsets = self.get_click_offsets()
         self.session_idx_arr = self.order_session_idx()
         
-        
     def get_click_offsets(self):
         """
         Return the offsets of the beginning clicks of each session IDs,
@@ -89,7 +67,6 @@ class SessionDataset:
         offsets[1:] = self.df.groupby(self.session_key).size().cumsum()
 
         return offsets
-    
 
     def order_session_idx(self):
         """ Order the session indices """
@@ -102,7 +79,6 @@ class SessionDataset:
             session_idx_arr = np.arange(self.df[self.session_key].nunique())
 
         return session_idx_arr
-    
     
     def add_item_indices(self, itemmap=None):
         """ 
@@ -120,13 +96,13 @@ class SessionDataset:
         self.itemmap = itemmap
         self.df = pd.merge(self.df, self.itemmap, on=self.item_key, how='inner')
         
-    
     @property    
     def items(self):
         return self.itemmap.ItemId.unique()
         
 
 class SessionDataLoader:
+    """Credit to yhs-968/pyGRU4REC."""    
     def __init__(self, dataset, batch_size=50):
         """
         A class for creating session-parallel mini-batches.
@@ -138,11 +114,10 @@ class SessionDataLoader:
         self.batch_size = batch_size
         self.done_sessions_counter = 0
         
-        
     def __iter__(self):
         """ Returns the iterator for producing session-parallel training mini-batches.
         Yields:
-            input (B,): torch.FloatTensor. Item indices that will be encoded as one-hot vectors later.
+            input (B,):  Item indices that will be encoded as one-hot vectors later.
             target (B,): a Variable that stores the target item indices
             masks: Numpy array indicating the positions of the sessions to be terminated
         """
@@ -165,7 +140,7 @@ class SessionDataLoader:
 
         while not finished:
             minlen = (end - start).min()
-            # Item indices(for embedding) for clicks where the first sessions start
+            # Item indices (for embedding) for clicks where the first sessions start
             idx_target = df.item_idx.values[start]
             for i in range(minlen - 1):
                 # Build inputs & targets
@@ -191,10 +166,6 @@ class SessionDataLoader:
                 end[idx] = click_offsets[session_idx_arr[maxiter] + 1]
 
 
-# In[ ]:
-
-
-# Modelo
 def create_model():   
     emb_size = 50
     hidden_units = 100
@@ -205,7 +176,6 @@ def create_model():
     drop2 = Dropout(0.25)(gru)
     predictions = Dense(n_items, activation='softmax')(drop2)
     model = Model(input=inputs, output=[predictions])
-    # lr original es 0.0001
     opt = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
     model.compile(loss=categorical_crossentropy, optimizer=opt)
     model.summary()
@@ -216,28 +186,19 @@ def create_model():
     return model
 
 
-# In[ ]:
-
-
 def get_states(model):
     return [K.get_value(s) for s,_ in model.state_updates]
+
 
 def set_states(model, states):
     for (d,_), s in zip(model.state_updates, states):
         K.set_value(d, s)
 
 
-# In[ ]:
-
-
-from sklearn.metrics import recall_score
-
 def get_recall(model, train_generator_map, recall_k=20):
-
 
     test_dataset = SessionDataset(test_data, itemmap=train_generator_map)
     test_generator = SessionDataLoader(test_dataset, batch_size=batch_size)
-
 
     n = 0
     suma = 0
@@ -245,14 +206,9 @@ def get_recall(model, train_generator_map, recall_k=20):
 
     for feat, label, mask in test_generator:
 
-        
-        print(feat)
-        
         input_oh = to_categorical(feat, num_classes=loader.n_items) 
         input_oh = np.expand_dims(input_oh, axis=1)
-
         target_oh = to_categorical(label, num_classes=loader.n_items)
-
         pred = model.predict(input_oh, batch_size=batch_size)
 
         if n%100 == 0:
@@ -275,9 +231,6 @@ def get_recall(model, train_generator_map, recall_k=20):
     print("Recall@{} epoch {}: {}".format(recall_k, epoch, suma/n))
 
 
-# In[ ]:
-
-
 def get_mrr(model, train_generator_map, mrr_k=20):
 
     test_dataset = SessionDataset(test_data, itemmap = train_generator_map)
@@ -291,7 +244,6 @@ def get_mrr(model, train_generator_map, mrr_k=20):
         input_oh = to_categorical(feat, num_classes=loader.n_items) 
         input_oh = np.expand_dims(input_oh, axis=1)
         target_oh = to_categorical(label, num_classes=loader.n_items)
-
         pred = model.predict(input_oh, batch_size=batch_size)
 
         if n%100 == 0:
@@ -312,9 +264,6 @@ def get_mrr(model, train_generator_map, mrr_k=20):
                 suma += 1/int((np.where(idx1 == idx2[0])[0]+1))        
 
     print("MRR@{} epoch {}: {}".format(mrr_k, epoch, suma/n))
-
-
-# In[ ]:
 
 
 def train_model(model, save_weights = False, path_to_weights = False):
@@ -357,42 +306,38 @@ def train_model(model, save_weights = False, path_to_weights = False):
                 model_to_train.save('./DwellTimeEpoch{}.h5'.format(epoch))
 
 
-# In[ ]:
-
-
 if __name__ == '__main__':
-    PATH_TO_TRAIN = '../DwellTimeTheano/augmented.csv'
+    PATH_TO_TRAIN = '../processedData/augmented.csv'
     PATH_TO_DEV = '../processedData/rsc15_train_valid.txt'
     PATH_TO_TEST = '../processedData/rsc15_test.txt'
     train_data = pd.read_csv(PATH_TO_TRAIN, sep='\t', dtype={'ItemId':np.int64})
     dev_data = pd.read_csv(PATH_TO_DEV, sep='\t', dtype={'ItemId':np.int64})
     test_data = pd.read_csv(PATH_TO_TEST, sep='\t', dtype={'ItemId': np.int64})
     
-    
-    batch_size = 512 #como en el paper
+    batch_size = 512
     session_max_len = 100
     embeddingp=False
 
     n_items = len(train_data['ItemId'].unique())+1
-    print("Items unicos training:", n_items)
+    print("Unique training items:", n_items)
 
     dev_n_items = len(dev_data['ItemId'].unique())+1
-    print("Items unicos dev:", dev_n_items)
+    print("Unique dev items:", dev_n_items)
 
     test_n_items = len(test_data['ItemId'].unique())+1
-    print("Items unicos testing:", test_n_items)
+    print("Unique testing items:", test_n_items)
 
-    train_samples_qty = len(train_data['SessionId'].unique()) # cantidad sesiones no augmentadas de train
-    print("Sesiones training:", train_samples_qty)
+    train_samples_qty = len(train_data['SessionId'].unique()) 
+    print("Training sessions:", train_samples_qty)
 
-    dev_samples_qty = len(dev_data['SessionId'].unique()) # cantidad sesiones no augmentadas de dev
-    print("Sesiones validation:",dev_samples_qty)
+    dev_samples_qty = len(dev_data['SessionId'].unique()) 
+    print("Dev sessions:",dev_samples_qty)
 
-    test_samples_qty = len(test_data['SessionId'].unique()) # cantidad sesiones no augmentadas de test
-    print("Sesiones testing:", test_samples_qty)
+    test_samples_qty = len(test_data['SessionId'].unique())
+    print("Testing sessions:", test_samples_qty)
     
-    train_fraction = 1#256 # 1/fraction es la cantidad de sesiones mas recientes a considerar
-    dev_fraction = 1#2
+    train_fraction = 1 # (1 / fraction) most recent session quantity to consider
+    dev_fraction = 1
 
     train_offset_step=train_samples_qty//batch_size
     dev_offset_step=dev_samples_qty//batch_size
---
+++ preprocess/rsc15-dataset-README.txt
@@ -0,0 +1,52 @@
+﻿SUMMARY
+================================================================================
+
+This dataset was constructed by YOOCHOOSE GmbH to support participants in the RecSys Challenge 2015.
+See  http://recsys.yoochoose.net for details about the challenge.
+
+The YOOCHOOSE dataset contain a collection of sessions from a retailer, where each session
+is encapsulating the click events that the user performed in the session.
+For some of the sessions, there are also buy events; means that the session ended
+with the user bought something from the web shop. The data was collected during several
+months in the year of 2014, reflecting the clicks and purchases performed by the users
+of an on-line retailer in Europe.  To protect end users privacy, as well as the retailer,
+all numbers have been modified. Do not try to reveal the identity of the retailer.
+
+LICENSE
+================================================================================
+This dataset is licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0
+International License. To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/.
+YOOCHOOSE cannot guarantee the completeness and correctness of the data or the validity
+of results based on the use of the dataset as it was collected by implicit tracking of a website. 
+If you have any further questions or comments, please contact YooChoose <support@YooChoose.com>. 
+The data is provided "as it is" and there is no obligation of YOOCHOOSE to correct it,
+improve it or to provide additional information about it.
+
+CLICKS DATASET FILE DESCRIPTION
+================================================================================
+The file yoochoose-clicks.dat comprising the clicks of the users over the items.
+Each record/line in the file has the following fields/format: Session ID, Timestamp, Item ID, Category
+-Session ID – the id of the session. In one session there are one or many clicks. Could be represented as an integer number.
+-Timestamp – the time when the click occurred. Format of YYYY-MM-DDThh:mm:ss.SSSZ
+-Item ID – the unique identifier of the item that has been clicked. Could be represented as an integer number.
+-Category – the context of the click. The value "S" indicates a special offer, "0" indicates  a missing value, a number between 1 to 12 indicates a real category identifier,
+ any other number indicates a brand. E.g. if an item has been clicked in the context of a promotion or special offer then the value will be "S", if the context was a brand i.e BOSCH,
+ then the value will be an 8-10 digits number. If the item has been clicked under regular category, i.e. sport, then the value will be a number between 1 to 12. 
+ 
+BUYS DATSET FILE DESCRIPTION
+================================================================================
+The file yoochoose-buys.dat comprising the buy events of the users over the items.
+Each record/line in the file has the following fields: Session ID, Timestamp, Item ID, Price, Quantity
+
+-Session ID - the id of the session. In one session there are one or many buying events. Could be represented as an integer number.
+-Timestamp - the time when the buy occurred. Format of YYYY-MM-DDThh:mm:ss.SSSZ
+-Item ID – the unique identifier of item that has been bought. Could be represented as an integer number.
+-Price – the price of the item. Could be represented as an integer number.
+-Quantity – the quantity in this buying.  Could be represented as an integer number.
+
+TEST DATASET FILE DESCRIPTION
+================================================================================
+The file yoochoose-test.dat comprising only clicks of users over items.
+This file served as a test file in the RecSys challenge 2015. 
+The structure is identical to the file yoochoose-clicks.dat but you will not find the
+corresponding buying events to these sessions in the yoochoose-buys.dat file.
